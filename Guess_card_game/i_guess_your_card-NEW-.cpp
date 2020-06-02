93da73c2d969efadc7554b55f4a1d10dbcc42c17
arfanamd <65768103+arfanamd@users.noreply.github.com>
2020-06-03 02:02:04 +0700

    Update i_guess_your_card-NEW-.cpp

--- Guess_card_game/i_guess_your_card-NEW-.cpp
+++ Guess_card_game/i_guess_your_card-NEW-.cpp
@@ -7,7 +7,32 @@
 
 #include "i_guess_your_card-NEW-.h"
 
-Stack_opt Stack_opt::Shuffle(Stack_opt *stack1, Stack_opt *stack2, Stack_opt *stack3) {
+Stack_opt::~Stack_opt(void) {}
+Stack_opt::Stack_opt(void) {
+	do {
+		for(unsigned int SHAKE = 0; SHAKE < CARDS_PER_STACK; SHAKE++) {
+			int _seed_ = sdt::rand();
+			cards[SHAKE] = (_seed_ % 13) + 2;	/* ace through king */
+			suits[SHAKE] = (_seed_ % CARDS_PER_STACK) + 1;	/* diamond, scope, etc. */
+			
+			cards_bak[SHAKE] = cards[SHAKE];
+			suits_bak[SHAKE] = suits[SHAKE];
+		}
+	} while(Compare());
+}
+bool Stack_opt::Compare(void) {
+	if(cards[0] == cards[1] && suits[0] == suits[1]) return true;
+	if(cards[0] == cards[2] && suits[0] == suits[2]) return true;
+	if(cards[0] == cards[3] && suits[0] == suits[3]) return true;
+	
+	if(cards[1] == cards[2] && suits[1] == suits[2]) return true;
+	if(cards[1] == cards[3] && suits[1] == suits[3]) return true;
+	
+	if(cards[2] == cards[3] && suits[2] == suits[3]) return true;
+	
+	return false;
+}
+void Stack_opt::Shuffle(Stack_opt *stack1, Stack_opt *stack2, Stack_opt *stack3) {
 /*-	stack 1 = 1{0, 3}, 2{2}, 3{1}	-*
  *-	stack 2 = 1{1}, 2{0,3}, 3{2}	-*
  *-	stack 3 = 1{2}, 2{1}, 3{0, 3}	-*/
@@ -45,84 +70,101 @@ Stack_opt Stack_opt::Shuffle(Stack_opt *stack1, Stack_opt *stack2, Stack_opt *st
 
 	return junk;
 }
+void Stack_opt::Show(void) {
+	for(unsigned int SHOW = 0; SHOW < CARDS_PER_STACK; SHOW++) {
+		Reveal(SHOW);
+	}
+}
+void Stack_opt::Reveal(unsigned int CARD) {
+	std::printf("|");
+	switch(suits[CARD]) {
+		case 1:
+			if(cards[CARD] <= 10) {
+				std::printf("\e[47;31m%2d♦ \e[0m", cards[CARD]);
+			} else {
+				switch(cards[CARD]) {
+					case 11:
+						std::printf("\e[47;31m J♦ \e[0m");
+						break;
+					case 12:
+						std::printf("\e[47;31m Q♦ \e[0m");
+						break;
+					case 13:
+						std::printf("\e[47;31m K♦ \e[0m");
+						break;
+					case 14:
+						std::printf("\e[47;31m A♦ \e[0m");
+						break;
+				}
+			}
+			break;
+		case 2:
+			if(cards[CARD] <= 10) {
+				std::printf("\e[47;31m%2d♥ \e[0m", cards[CARD]);
+			} else {
+				switch(cards[CARD]) {
+					case 11:
+						std::printf("\e[47;31m J♥ \e[0m");
+						break;
+					case 12:
+						std::printf("\e[47;31m Q♥ \e[0m");
+						break;
+					case 13:
+						std::printf("\e[47;31m K♥ \e[0m");
+						break;
+					case 14:
+						std::printf("\e[47;31m A♥ \e[0m");
+						break;
+				}
+			}
+			break;
+		case 3:
+			if(cards[CARD] <= 10) {
+				std::printf("\e[47;30m%2d♣ \e[0m", cards[CARD]);
+			} else {
+				switch(cards[CARD]) {
+					case 11:
+						std::printf("\e[47;30m J♣ \e[0m");
+						break;
+					case 12:
+						std::printf("\e[47;30m Q♣ \e[0m");
+						break;
+					case 13:
+						std::printf("\e[47;30m K♣ \e[0m");
+						break;
+					case 14:
+						std::printf("\e[47;30m A♣ \e[0m");
+						break;
+				}
+			}
+			break;
+		case 4:
+			if(cards[CARD] <= 10) {
+				std::printf("\e[47;30m%2d♠ \e[0m", cards[CARD]);
+			} else {
+				switch(cards[CARD]) {
+					case 11:
+						std::printf("\e[47;30m J♠ \e[0m");
+						break;
+					case 12:
+						std::printf("\e[47;30m Q♠ \e[0m");
+						break;
+					case 13:
+						std::printf("\e[47;30m K♠ \e[0m");
+						break;
+					case 14:
+						std::printf("\e[47;30m A♠ \e[0m");
+						break;
+				}
+			}
+			break;
+	}
+	std::printf("|");
+}
 
 void Sleep(int duration) {
 	std::this_thread::sleep_for(std::chrono::milliseconds(duration * 1000));
 }
-
 void Clean_screen(void) {
 	system("clear");
 }
-
-void Play_game(Stack_opt &stack1, Stack_opt &stack2, Stack_opt &stack3) {
-	Stack_opt junk;
-
-	char ent_key[2];
-	unsigned short int pick = 1;
-	unsigned short int stat = 0;
-
-	Clean_screen();
-
-	stack1.Show(); std::puts("\n");
-	stack2.Show(); std::puts("\n");
-	stack3.Show(); std::puts("\n");
-
-	std::printf("   --: Pick one card! :--\n");
-	std::printf("just remember it and press enter");
-	std::fgets(ent_key, sizeof(ent_key), stdin);
-	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-
-	while( 1 ) {
-		if(pick == 1) {
-			junk = junk.Shuffle(&stack1, &stack2, &stack3);
-		} else if(pick == 2) {
-			junk = junk.Shuffle(&stack2, &stack1, &stack3);
-		} else if(pick == 3 && stat <= 1) {
-			junk = junk.Shuffle(&stack3, &stack1, &stack2);
-		} else if(pick == 3 && stat >= 2) {
-			break;
-		} else {
-			std::puts("wth dude??..");
-			Sleep(2);
-			goto UNEDUCATED_PLAYER;
-		}
-
-		Clean_screen();
-
-		stack1.Show(); std::printf(" (1)\n\n");
-		stack2.Show(); std::printf(" (2)\n\n");
-		stack3.Show(); std::printf(" (3)\n\n");
-
-		Sleep(1);
-
-		std::puts("in wich stack your card is showin' up?");
-		std::printf("[1, 2, or 3] ? ");
-		std::cin >> pick;
-		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-
-		stat++;
-	}
-
-	Clean_screen();
-	std::printf("Your card is ");
-	Sleep(1);
-	stack3.Reveal(0);
-	std::putchar('\n');
-
-UNEDUCATED_PLAYER:
-	junk.~Stack_opt();
-}
-
-int main(void) {
-	std::srand(std::time(0));
-
-	Stack_opt stack1, stack2, stack3;
-	Play_game(stack1, stack2, stack3);
-
-	stack1.~Stack_opt();
-	stack2.~Stack_opt();
-	stack3.~Stack_opt();
-
-	return 0;
-}
-
